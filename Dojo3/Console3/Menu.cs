9eedbec9a6d28c0141c93e235f69ba123ccd6797
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-07-20 08:41:46 -0300

    Cabecalho

---
+++ Dojo3/Console3/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console3
+{
+    public class Menu
+    {
+        public void Cabecalho()
+        {
+            Console.WriteLine("************************Bem vindo**************************" +
+                            "\n***********************************************************");
+            Console.ReadLine();
+        }
+    }
+}
