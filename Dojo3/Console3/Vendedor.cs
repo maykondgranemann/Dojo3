2298c7e52f9813cefc39e20c5bd48946a509ff77
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-07-20 08:52:23 -0300

    Classe Vendedor adicionada

---
+++ Dojo3/Console3/Vendedor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console3
+{
+    class Vendedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public string Setor { get; set; }
+
+
+        public Vendedor()
+        {
+
+        }
+
+        public Vendedor(int id, string nome, string matricula, string setor)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Matricula = matricula;
+            this.Setor = setor;
+        }
+    }
+}
