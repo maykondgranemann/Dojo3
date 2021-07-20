3a8c5caaa7ea54d98e4c3b3a98f2f967a836eaf8
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-07-20 09:01:51 -0300

    Implementando metodos CRUD

---
+++ Dojo3/Console3/Operacoes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console3
+{
+    class Operacoes
+    {
+        Produto produto = new Produto();
+        public void CadastrarProduto()
+        {
+            Console.WriteLine("Insira as ID do produto");
+            produto.ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o nome do produto");
+            produto.Nome = Console.ReadLine();
+            Console.WriteLine("Insira o valor do produto");
+            produto.Valor = Convert.ToDecimal(Console.ReadLine());
+
+        }
+        public void Update(int id)
+        {
+
+        }
+        public void Delete(int id)
+        {
+
+        }
+        public void Listar()
+        {
+
+        }
+    }
+}
