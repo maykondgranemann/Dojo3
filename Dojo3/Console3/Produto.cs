ba235627bd93fb010dab56a56614ca896e5ff514
Maykon D. Granemann <maykondgranemann@gmail.com>
2021-07-20 08:46:36 -0300

    Add classe Produto

---
+++ Dojo3/Console3/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console3
+{
+    class Produto
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(int id,string nome,decimal valor)
+        {
+            this.ID = id;
+            this.Nome = nome;
+            this.Valor = valor;
+        }
+
+    }
+}
